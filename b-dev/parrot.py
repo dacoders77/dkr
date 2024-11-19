6805d6780bcf32a409db2da479975bcbb1a326c3
Boris Borisov <djslinger77@gmail.com>
2024-11-19 17:53:26 -0500

    Dog class. First commit to Dog branch

--- b-dev/parrot.py
+++ b-dev/parrot.py
@@ -1,7 +1,7 @@
-import numpy as np
+class Dog:
+    def bark(self):
+        print("bark")
 
-arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+d = Dog()
 
-print(arr)
 
-# Deleted all comments. Head and everthing. Commited and pushed 
