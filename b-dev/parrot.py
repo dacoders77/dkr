2091517892bb1401b136eacdb7880556ef691cd9
Boris Borisov <djslinger77@gmail.com>
2024-11-20 17:55:35 -0500

    Added memory limit for bor-ubu container

--- b-dev/parrot.py
+++ b-dev/parrot.py
@@ -1,7 +1,23 @@
 class Dog:
-    def bark(self):
-        print("bark")
 
-d = Dog() # Upstream?
+    def __init__(self, name):
+        self.name = name
+        print("Constructor called!")
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return 99
+
+    # We can access variables / properties set in the __init__ constructor
+    def set_age(self, age):
+        self.age = age
 
 
+
+d = Dog("Lucky")
+d2 = Dog("Bill")
+d.set_age(99)
+
+print(f"1st object name: {d.get_name()}. 1st object age: {d.get_age()}")
