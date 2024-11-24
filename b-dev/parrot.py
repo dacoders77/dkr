e069a3b4aa62407249ad00a005e2af54ab940317
Boris Borisov <djslinger77@gmail.com>
2024-11-24 14:43:39 -0500

    Deleted all the code. This is branch Cat

--- b-dev/parrot.py
+++ b-dev/parrot.py
@@ -1,27 +1,2 @@
-"""
-make 3 classes:
-animal - inherits from fish and dog
-the only constructor is predent in fish
-"""
-
-class Dog:
-    def __init__(self):
-        print("Animal constructor called")
-    def bark(self):
-        pass
-
-class Fish:
-    def swim(self):
-        print("Swim from fish")
-
-class Animal(Dog, Fish):
-    def animal(self):
-        pass
-    def swim(self):
-        print("swimm from animal")
-
-a = Animal()
-a.swim()
-Fish.swim(a) # Calling swim method, which is overridden if Animal is created
-
-
+# Deleted all the code
+# Commited from branch Cat
